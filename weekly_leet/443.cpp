0eb306008d3f560b26cb9ab947061051f26901d3
harim0 <achel0303@naver.com>
2025-04-03 21:49:43 +0900

    add first

---
+++ weekly_leet/443.cpp
@@ -0,0 +1,12 @@
+// 3502. Minimum Cost to Reach Every Position
+// https://leetcode.com/contest/weekly-contest-443/problems/minimum-cost-to-reach-every-position/
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> minCosts(vector<int>& cost) {
+        
+    }
+};
